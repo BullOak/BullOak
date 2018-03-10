5a4d1bf112d985723f29d0314f87cc31612f4e26
Ivan Blazhko <ivan.blazhko@gmail.com>
2018-03-10 13:32:29 +0000

    - Added NuGet to source control - Removed FAKE from source control, getting it via NuGet - Extended `build.fsx` to build and run all the tests - Added `build.ps1` botstrapper script - Added `xunit.runner.VisualStudio` to all test projects - this is required for `dotnet test` - Fixed build warning by removing some assemblies redirects

--- build.fsx
+++ build.fsx
@@ -1,14 +1,109 @@
-// include Fake lib
+#r @"./packages/FAKE.4.64.10/tools/FakeLib.dll"
 
-#r @"fake/FakeLib.dll"
 open Fake
+open System
+open System.Diagnostics
+open System.IO
 
-let buildDir = "./build/"
+module Properties =
+    let buildConfiguration = getBuildParamOrDefault "Configuration" "Release"
 
-Target "Clean" (fun _ ->
-  CleanDir buildDir
-  trace "Cleaned buildDir")
+    module Internal =
 
+        // Absolute path to solution locations
+        let repositoryDir = DirectoryInfo(__SOURCE_DIRECTORY__).FullName
+        let packagesDir = Path.Combine(repositoryDir, "packages")
+        let sourceDir = Path.Combine(repositoryDir, "src")
+        let solutionFile = Path.Combine(sourceDir, "BullOak.sln")
 
-// start build
-RunTargetOrDefault "Clean"
+        // Tests
+        let testsProjectPathPatternByType = fun t -> sprintf @"%s\**\*.Test.%s.csproj" sourceDir t
+        let runTestsInProject = fun x -> DotNetCli.Test (fun p ->
+            {   p with
+                    Project = x
+                    Configuration = buildConfiguration
+                    AdditionalArgs = [ "--logger"; "trx"; ]
+            })
+
+///////////////////////////////////////////////////////////////////////
+// Targets
+///////////////////////////////////////////////////////////////////////
+
+module Targets =
+    open Properties
+    open Properties.Internal
+
+    Target "Purge" (fun _ ->
+        let script = sprintf "$startPath = '%s'; Get-ChildItem -Path $startPath -Filter 'bin' -Directory -Recurse | Foreach { $_.Delete($true) }; Get-ChildItem -Path $startPath -Filter 'obj' -Directory -Recurse | Foreach { $_.Delete($true) }" repositoryDir
+        let powershellWrapper = sprintf """/c powershell -ExecutionPolicy Unrestricted -Command "%s" """ script
+
+        let p = new Process()
+        p.StartInfo.FileName <- "cmd.exe"
+        p.StartInfo.Arguments <- powershellWrapper
+        p.StartInfo.RedirectStandardOutput <- true
+        p.StartInfo.UseShellExecute <- false
+        p.Start() |> ignore
+
+        printfn "%s" (p.StandardOutput.ReadToEnd())
+    )
+
+    Target "Clean" (fun _ ->
+        DotNetCli.RunCommand (fun p ->
+             { p with
+                 TimeOut = TimeSpan.FromMinutes 10.
+             }) (sprintf "clean \"%s\"" solutionFile)
+    )
+
+    Target "Restore" (fun _ ->
+        solutionFile
+        |> RestoreMSSolutionPackages (fun p ->
+            {   p with
+                    OutputPath = packagesDir
+                    Retries = 3
+            })
+    )
+
+    Target "Build" (fun _ ->
+        DotNetCli.Build (fun p ->
+            { p with
+                Project = solutionFile
+                Configuration = buildConfiguration
+            })
+    )
+
+    Target "UnitTests" (fun _ ->
+        !! (testsProjectPathPatternByType "Unit")
+        |> Seq.iter runTestsInProject
+    )
+
+    Target "AcceptanceTests" (fun _ ->
+        !! (testsProjectPathPatternByType "Acceptance")
+        |> Seq.iter runTestsInProject
+    )
+
+    Target "IntegrationTests" (fun _ ->
+        !! (testsProjectPathPatternByType "Integration")
+        |> Seq.iter runTestsInProject
+    )
+
+    // Labels to manage dependencies
+    Target "FullBuild" DoNothing
+    Target "Default" DoNothing
+
+///////////////////////////////////////////////////////////////////////
+// Dependencies
+///////////////////////////////////////////////////////////////////////
+
+"Restore" ==> "Build"
+"Build" ==> "UnitTests"
+"Build" ==> "AcceptanceTests"
+"Build" ==> "IntegrationTests"
+"UnitTests" ==> "FullBuild"
+"AcceptanceTests" ==> "FullBuild"
+"IntegrationTests" ==> "FullBuild"
+
+// Default target
+"FullBuild" ==> "Default"
+
+// Start
+RunTargetOrDefault "Default"
---
+++ build.ps1
@@ -0,0 +1,27 @@
+Param(
+    [ValidateNotNullOrEmpty()]
+    [string]$Target="Default",
+
+    [ValidateNotNullOrEmpty()]
+    [ValidateSet("Debug", "Release")]
+    [string]$Configuration="Release"
+)
+
+$nugetVersion="4.5.1"
+$fakeVersion="4.64.10"
+
+$buildDir=$PSScriptRoot
+$buildScript=[System.IO.Path]::Combine($buildDir, "build.fsx")
+
+$solutionDir=(Get-Item $buildDir).FullName
+$nugetPackagesDir=[System.IO.Path]::Combine($solutionDir, "packages")
+$toolsDir=[System.IO.Path]::Combine($solutionDir, "tools")
+$nuget=[System.IO.Path]::Combine($toolsDir, "NuGet-$nugetVersion", "nuget.exe")
+$fake=[System.IO.Path]::Combine($nugetPackagesDir, "FAKE.$fakeVersion", "tools", "FAKE.exe")
+
+Write-Host -ForegroundColor Green "*** Building $Configuration in $solutionDir"
+
+& "$nuget" install FAKE -OutputDirectory $nugetPackagesDir -Version $fakeVersion -Verbosity quiet
+
+Write-Host -ForegroundColor Green "***    FAKE it"
+& "$fake" "$buildScript" "$Target" Configuration="$Configuration"

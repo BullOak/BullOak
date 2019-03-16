50e47ecc5596ab4e75dc405e51320c3859aee12d
Savvas Kleanthous <skleanthous@outlook.com>
2019-03-16 19:56:31 +0000

    BO-118: Added event metadata for ES integration and changed deserialization to use FQN

---
+++ src/BullOak.Repositories.EventStore/MetadataFactory.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace BullOak.Repositories.EventStore
+{
+    using Newtonsoft.Json;
+
+    internal class MetadataFactory
+    {
+        internal static (IHoldMetadata metadata,int version) GetMetadataFrom(int metadataVersion, JObject asJson)
+        {
+            switch (metadataVersion)
+            {
+                case 1:
+                    return (asJson.ToObject<EventMetadata_V1>(), 1);
+                default:
+                    throw new MetadataException(asJson, $"Unrecognized Metadata Version: {metadataVersion}");
+            }
+        }
+    }
+}
